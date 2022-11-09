7ea5327a40ce688cd6b5f51f2f129b411ea03cf0
YosophAhmed <devyosoph@gmail.com>
2022-11-09 22:11:47 +0200

    App Theme

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notes/views/notes_view.dart';
+import 'package:sizer/sizer.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            brightness: Brightness.dark,
+          ),
+          home: const NotesView(),
+        );
+      },
+    );
+  }
+}
