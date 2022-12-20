ce084cbfac1044ea4defc043809b0f2b9acab2f5
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-20 23:12:55 +0200

    Add Bloc Provider

--- lib/notes_app.dart
+++ lib/notes_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/add_note_cubit/add_notes_cubit.dart';
 import 'package:notes/views/notes_view.dart';
 import 'package:sizer/sizer.dart';
 
@@ -11,17 +13,24 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-      builder: (context, orientation, deviceType) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            brightness: Brightness.dark,
-            fontFamily: 'Poppins',
-          ),
-          home: const NotesView(),
-        );
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNotesCubit(),
+        ),
+      ],
+      child: Sizer(
+        builder: (context, orientation, deviceType) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              brightness: Brightness.dark,
+              fontFamily: 'Poppins',
+            ),
+            home: const NotesView(),
+          );
+        },
+      ),
     );
   }
 }
