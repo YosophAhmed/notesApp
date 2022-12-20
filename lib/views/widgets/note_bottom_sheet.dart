12b9954c0e8befba6db638064ee8108a9b03e660
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-20 23:13:18 +0200

    Add Bloc Consumer

--- lib/views/widgets/note_bottom_sheet.dart
+++ lib/views/widgets/note_bottom_sheet.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:notes/views/widgets/custom_botton.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/add_note_cubit/add_notes_cubit.dart';
+import 'package:notes/cubits/add_note_cubit/add_notes_states.dart';
 import 'package:sizer/sizer.dart';
 
-import 'custom_text_field.dart';
+import 'add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({
@@ -17,67 +19,15 @@ class AddNoteBottomSheet extends StatelessWidget {
       padding: EdgeInsets.symmetric(
         horizontal: 3.5.w,
       ),
-      child: const AddNoteForm(),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({Key? key}) : super(key: key);
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-
-  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
-
-  String? title, subTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autoValidateMode,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 4.h,
-          ),
-          CustomTextField(
-            hintText: 'Title',
-            onSaved: (value) {
-              title = value;
-            },
-          ),
-          SizedBox(
-            height: 4.h,
-          ),
-          CustomTextField(
-            hintText: 'Content',
-            onSaved: (value) {
-              subTitle = value;
-            },
-            maxLines: 5,
-          ),
-          SizedBox(
-            height: 4.h,
-          ),
-          CustomButton(
-            label: 'Add',
-            height: 8.h,
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autoValidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-        ],
+      child: BlocConsumer<AddNotesCubit, AddNotesState>(
+        listener: (context, state) {
+          if(state is SuccessAddNotesState) {
+            Navigator.pop(context);
+          }
+        },
+        builder: (context, state) {
+          return const AddNoteForm();
+        },
       ),
     );
   }
