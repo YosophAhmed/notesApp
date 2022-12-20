14e7a31a108e44b47b3f9ea3ccd3992dd7829576
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-20 03:56:21 +0200

    Initialise Hive

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xff62FCD7);
+
+const String kNotesBox = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes/constants/colors.dart';
 import 'notes_app.dart';
 
-void main() {
+void main() async {
+
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+
   runApp(
     const NotesApp(),
   );
